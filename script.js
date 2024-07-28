bdce43001c16c164a2b996cb4efc5b9e05f32857
RobloxGov <161930163+RobloxGov@users.noreply.github.com>
2024-07-28 12:56:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function toggleMenu() {
     const navList = document.querySelector('.nav-list');
     const hamburgerMenu = document.querySelector('.hamburger-menu');
-    
+    const body = document.body; // ใช้ body แทน document.body
+
     navList.classList.toggle('active');
-    hamburgerMenu.classList.toggle('active'); // เพิ่มการเปลี่ยนแปลงสถานะ
+    hamburgerMenu.classList.toggle('active');
+
+    // เปลี่ยนแปลงสถานะ overflow ของ body
+    if (navList.classList.contains('active')) {
+        body.style.overflow = 'hidden'; // ปิดการเลื่อนเมื่อเมนูเปิด
+    } else {
+        body.style.overflow = ''; // กลับไปเป็นค่าเริ่มต้นเมื่อเมนูปิด
+    }
 }
